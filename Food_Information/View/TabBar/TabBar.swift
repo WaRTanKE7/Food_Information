1c2b4fda17993a66b497f788f8d425004bb7e26d
E7 <a24833870@gmail.com>
2022-05-31 17:15:08 +0800

    Adjusted – TabBar 樣式

--- Food_Information/View/TabBar/TabBar.swift
+++ Food_Information/View/TabBar/TabBar.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 enum Tab: String, CaseIterable {
-    case Search = "magnifyingglass"
-    case Home = "house.fill"
-    case Favorite = "bookmark.fill"
-    case Acount = "person.fill"
+    case Search = "搜尋"
+    case Home = "首頁"
+    case Favorite = "收藏"
+    case Acount = "個人"
 }
 
 struct TabBar: View {
@@ -31,35 +31,42 @@ struct TabBar: View {
         TabView(selection: $currentTab) {
             Text("搜尋")
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.white.ignoresSafeArea())
+                .background(Color.gray.opacity(0.1).ignoresSafeArea())
                 .tag(Tab.Search)
             
             Home().body
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.white.ignoresSafeArea())
+                .background(Color.gray.opacity(0.1).ignoresSafeArea())
                 .tag(Tab.Home)
             
             Text("我的收藏")
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.white.ignoresSafeArea())
+                .background(Color.gray.opacity(0.1).ignoresSafeArea())
                 .tag(Tab.Favorite)
             
             Text("個人")
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.white.ignoresSafeArea())
+                .background(Color.gray.opacity(0.1).ignoresSafeArea())
                 .tag(Tab.Acount)
         }
         .overlay(
             HStack(spacing: 0) {
                 ForEach(Tab.allCases, id: \.rawValue) { tab in
                     TabButton(tab: tab)
+                        .overlay {
+                            Text(tab.rawValue)
+                                .font(.system(size: 14, weight: .semibold))
+                                .foregroundColor(.black)
+                                .offset(y: currentTab == tab ? 15 : 100)
+                        }
                 }
             }
             .padding(.vertical)
             // 全螢幕手機 SafeArea Bottom != 0 要減少，以免 TabBar 使用太多空間
             .padding(.bottom, getSafeArea().bottom == 0 ? 10 : (getSafeArea().bottom - 10))
             .background(
-                MaterialEffect(style: .systemUltraThinMaterialLight)
+                Color
+                    .white
                     .clipShape(BottomCurve(currentXValue: currentXValue))
             )
             ,alignment: .bottom
@@ -95,18 +102,20 @@ struct TabBar: View {
                     currentXValue = proxy.frame(in: .global).midX
                 }
             } label: {
-                Image(systemName: tab.rawValue)
+                Image(tab.rawValue)
+                    .renderingMode(.template)
                     .resizable()
+                    .foregroundColor(currentTab == tab ? .white : .gray)
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 25, height: 25)
                     .frame(maxWidth: .infinity)
-                    .foregroundColor(currentTab == tab ? .white : .black)
                     .padding(currentTab == tab ? 15 : 0)
                     .background(
                         ZStack {
                             if currentTab == tab {
-                                MaterialEffect(style: .systemUltraThinMaterialDark)
-                                    .clipShape(Circle())
+                                Circle()
+                                    .fill(Color("orange"))
+                                    .shadow(color: Color.gray.opacity(0.8), radius: 8, x: 0, y: 10)
                                     .matchedGeometryEffect(id: "TAB", in: animation)
                             }
                         }
